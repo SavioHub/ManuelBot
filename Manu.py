ddc0e17db8e14fad12d504dfc3fb1605b6e95c0a
Alisson santos <craftgames.alysson@gmail.com>
2020-12-03 11:04:16 -0300

    email HTML

--- Manu.py
+++ Manu.py
@@ -1,34 +1,35 @@
 # import necessary packages
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import email.message
+from email import charset
 import smtplib
 
 #create message object instance
 
-msg = MIMEMultipart()
+html = open('email.html', 'r')
 
-message = 'att Internet Check!'
+charset.add_charset('utf-8', charset.QP, charset.QP, 'utf-8')
+msg = email.message.Message()
 
 #configurar os parâmetros da mensagem
 
 password = "987654321SAJ"
-msg['From'] = "internetcheck@gmail.com"
-msg['To'] = "saviosantossilva19@gmail.com"
-msg['Subject'] = "Olá, bom dia!.\n A internet da comunidade de Riachão Malhada BA está muito ruim e com alta instabilidade"
-
-#adicionar no corpo da mensagem
-
-msg.attach(MIMEText(message, 'plain'))
+msg['From'] = "checkinternet2021@gmail.com"
+msg['To'] = "craftgames.alysson@gmail.com"
+msg['Subject'] = "Aviso de instabilidade"
+msg.add_header('content-type', 'text/html')
+msg.set_charset('utf8')
+msg.get_content_charset()
+msg.set_payload(html.read().encode('utf8'))
 
 #create server
-server = smtplib.SMTP('smatp.gmail.com:587')
+server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
 
 #Credenciais de login para enviar o e-mail
-server.login(msg['from', password])
+server.login(msg['from'], password)
 
 # send the message via the server.
 server.sendmail(msg['from'], msg['To'], msg.as_string()) 
 
 server.quit()
-print("successfully sent email to %s:" % (msg['To']))
+print("successfully sent email to "+msg['To']+":")
