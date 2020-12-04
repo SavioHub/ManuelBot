9afa6c79c74d1bd605eb44188c870013d3761408
Alisson santos <craftgames.alysson@gmail.com>
2020-12-04 07:26:50 -0300

    reistruturação do script

--- libs/filaMail.py
+++ libs/filaMail.py
@@ -1,22 +1,22 @@
 class Queue:
-2	    def __init__(self):
-3	        self.items = []
-4	
-5	    def isEmpty(self):
-6	        return self.items == []
-7	
-8	    def enqueue(self, item):
-9	        self.items.insert(0,item)
-10	
-11	    def dequeue(self):
-12	        return self.items.pop()
-13	
-14	    def size(self):
-15	        return len(self.items)
-16	
-17	q=Queue()
-18	
-19	q.enqueue(4)
-20	q.enqueue('dog')
-21	q.enqueue(True)
-22	print(q.size())
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q = Queue()
+
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+print(q.size())
